06ed109d536ed4066e35a9e62a40089b5e44490b
yashsuhagiya <yashsuhagiya401@gmail.com>
2020-08-16 10:45:22 +0530

    models.py change

--- shop/models.py
+++ shop/models.py
@@ -1,27 +1,59 @@
 from django.db import models
 from django.contrib.auth.models import User
+from dj.choices import Choice, Choices
+from dj.choices.fields import ChoiceField
 # Create your models here.
-class Customer(models.Model):
-	user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
-	name = models.CharField(max_length=200,null=True)
-	email = models.CharField(max_length=200,null=True)
 
-	def __str__(self):
-		return self.name
+class Gender(Choices):
+	male = Choice("male")
+	female = Choice("female")
+	not_spacified = Choice("not specified")
+
+class Address(models.Model):
+	address_id = models.AutoField()
+	first_len = models.CharField(max_length=200,null=False)
+	second_len = models.CharField(max_length=200,null=True)
+	city = models.CharField(max_length=200)
+	state = models.CharField(max_length=30)
+	pincode = models.IntegerField(max_length=6)
+	primary_address = models.BooleanField(default=False)
+
+class Customer(models.Model):
+	customer_id = models.AutoField(primary_key=True)
+	firstname = models.CharField(max_length=200,null=False)
+	lastname = models.CharField(max_length=200,null=False)
+	email = models.CharField(max_length=200,null=False)
+	password = models.CharField(max_length=15,null=False)
+	gender = models.Choices(choices=Gender,default=Gender.not_spacified)
+	address_id = models.ForeignKey(Address, on_delete=models.CASCADE, null=False, blank=True)
+	birthdate = models.DateField()
+	mobile_no = models.IntegerField(max_length=10)
 
 class Product(models.Model):
+	product_id = models.AutoField(primary_key=True)
 	name = models.CharField(max_length=200,null=True)
-	price = models.FloatField()
+	price = models.DecimalField()
 	image = models.ImageField(null=True, blank=True)
-
-	def __str__(self):
-		return self.name 
+	category = models.CharField(max_length=30)
+	stock = models.IntegerField()
 
 class Order(models.Model):
-	customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
+	order_id = models.AutoField(primary_key=True)
+	customer_id = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
 	date_ordered = models.DateTimeField(auto_now_add=True)
-	complete = models.BooleanField(default=False)
-	transaction_id = models.CharField(max_length=100, null=True)
+	total_amount = models.DecimalField()
+	product_id = models.ForeignKey(Product, on_delete=models.SET_NULL, null=False, blank=True)
+	address_id = models.ForeignKey(Address, on_delete=models.SET_NULL, null=False, blank=True)
 
-	def __str__(self):
-		return self.name
+class customer_search(models.Model):
+	customer_id = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
+	search = models.CharField()
+
+class shopping_cart(models.Model):
+	product_id = models.ForeignKey(Product, on_delete=models.SET_NULL, null=False, blank=True)
+	quantity = models.IntegerField()
+	customer_id = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
+
+class admin_detail(models.Model):
+	username = models.CharField(max_length=200)
+	password = models.CharField(max_length=15)
