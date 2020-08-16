6cbafacc0637936db363326937cb5112683d7405
paras <parassuvagiya987@gmail.com>
2020-08-16 16:21:47 +0530

    q

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'test1',
+        'NAME': 'EcommerceDB',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': '127.0.0.1',
