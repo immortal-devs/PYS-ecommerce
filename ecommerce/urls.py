7520fa2c26d502c3bd1908d53608b9e75e35825e
yashsuhagiya <yashsuhagiya401@gmail.com>
2020-08-16 19:18:37 +0530

    change

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -6,7 +6,7 @@ from django.conf import settings
 
 urlpatterns = [
    # path('admin/', admin.site.urls),
-    path('shop',include('shop.urls')),
+    path('',include('shop.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
