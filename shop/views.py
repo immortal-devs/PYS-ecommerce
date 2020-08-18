1c52269f24776d30475c64b3622137fe6be8ddb9
yashsuhagiya <yashsuhagiya401@gmail.com>
2020-08-18 12:05:13 +0530

    change

--- shop/views.py
+++ shop/views.py
@@ -7,7 +7,7 @@ from django.db import IntegrityError
 import math, random
 
 # Create your views here.
-from .models import *
+from .models import Address, Admin_detail, Customer, Order, OrderItem, Product, shopping_cart, UserProfile
 
 def product(request):
 	products = Product.objects.all()
