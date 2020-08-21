1c628efeef8e5d6ee48dde7bd16ace9a11ff8ee1
yashsuhagiya <yashsuhagiya401@gmail.com>
2020-08-21 18:26:37 +0530

    change in logo

--- shop/views.py
+++ shop/views.py
@@ -16,14 +16,7 @@ def product(request):
 	return render(request, 'product.html', context)
 
 def cart(request):
-    if request.user.is_authenticated:
-        customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer=customer, complete=False)
-        items = order.orderitem_set.all()
-    else:
-        items = []
-        order = {'get_cart_total':0,'get_cart_items':0}
-    context = {'items':items, 'order':order}
+    context = {}
     return render(request, 'cart.html', context)
 
 def checkout(request):
