df14e5e636c678861fcbd004fdf8073b87938832
paras <parassuvagiya987@gmail.com>
2020-09-09 07:20:07 +0530

    cart

--- shop/urls.py
+++ shop/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
 	path('category/', views.category, name="category"),
 	path('product/', views.product, name="product"),
 	path('cart/', views.cart, name="cart"),
+	path('contact/', views.contact, name="contact"),
 	path('checkout/', views.checkout, name="checkout"),
 	path('', views.shop, name="shop"),
 	path('shop/', views.shop, name="shop"),
